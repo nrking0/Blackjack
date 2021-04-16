1e7346bbee11e68c6942814fd57199b032017905
Nick King <nrking0@gmail.com>
2021-04-15 21:33:26 -0500

    Worked to clean up formatting and documentation

--- src/card.cc
+++ src/card.cc
@@ -2,80 +2,81 @@
 
 namespace blackjack {
 
-    Card::Card(Suit suit, Rank rank) {
-        this->rank_ = rank;
-        this->suit_ = suit;
-    }
-
-    Rank Card::GetRank() const {
-        return this->rank_;
-    }
-
-    Suit Card::GetSuit() const {
-        return this->suit_;
-    }
-
-    std::ostream& operator<<(std::ostream& os, Card& card) {
-        std::string suit;
-        std::string rank;
-        switch (card.GetSuit()) {
-            case Suit::CLUBS:
-                suit = "Clubs";
-                break;
-            case Suit::HEARTS:
-                suit = "Hearts";
-                break;
-            case Suit::SPADES:
-                suit = "Spades";
-                break;
-            case Suit::DIAMONDS:
-                suit = "Diamonds";
-                break;
-        }
-
-        switch (card.GetRank()) {
-            case Rank::ACE:
-                rank = "Ace";
-                break;
-            case Rank::TWO:
-                rank = "Two";
-                break;
-            case Rank::THREE:
-                rank = "Three";
-                break;
-            case Rank::FOUR:
-                rank = "Four";
-                break;
-            case Rank::FIVE:
-                rank = "Five";
-                break;
-            case Rank::SIX:
-                rank = "Six";
-                break;
-            case Rank::SEVEN:
-                rank = "Seven";
-                break;
-            case Rank::EIGHT:
-                rank = "Eight";
-                break;
-            case Rank::NINE:
-                rank = "Nine";
-                break;
-            case Rank::TEN:
-                rank = "Ten";
-                break;
-            case Rank::JACK:
-                rank = "Jack";
-                break;
-            case Rank::QUEEN:
-                rank = "Queen";
-                break;
-            case Rank::KING:
-                rank = "King";
-                break;
-        }
-        os << rank + " of " + suit << std::endl;
-        return os;
-    }
+Card::Card(Suit suit, Rank rank) {
+    this->rank_ = rank;
+    this->suit_ = suit;
 }
+
+Rank Card::GetRank() const {
+    return this->rank_;
+}
+
+Suit Card::GetSuit() const {
+    return this->suit_;
+}
+
+std::ostream& operator<<(std::ostream& os, Card& card) {
+    std::string suit;
+    std::string rank;
+    switch (card.GetSuit()) {
+        case Suit::CLUBS:
+            suit = "Clubs";
+            break;
+        case Suit::HEARTS:
+            suit = "Hearts";
+            break;
+        case Suit::SPADES:
+            suit = "Spades";
+            break;
+        case Suit::DIAMONDS:
+            suit = "Diamonds";
+            break;
+    }
+
+    switch (card.GetRank()) {
+        case Rank::ACE:
+            rank = "Ace";
+            break;
+        case Rank::TWO:
+            rank = "Two";
+            break;
+        case Rank::THREE:
+            rank = "Three";
+            break;
+        case Rank::FOUR:
+            rank = "Four";
+            break;
+        case Rank::FIVE:
+            rank = "Five";
+            break;
+        case Rank::SIX:
+            rank = "Six";
+            break;
+        case Rank::SEVEN:
+            rank = "Seven";
+            break;
+        case Rank::EIGHT:
+            rank = "Eight";
+            break;
+        case Rank::NINE:
+            rank = "Nine";
+            break;
+        case Rank::TEN:
+            rank = "Ten";
+            break;
+        case Rank::JACK:
+            rank = "Jack";
+            break;
+        case Rank::QUEEN:
+            rank = "Queen";
+            break;
+        case Rank::KING:
+            rank = "King";
+            break;
+    }
+    os << rank + " of " + suit << std::endl;
+    return os;
+}
+
+} // namespace blackjack
 
--- src/deck.cc
+++ src/deck.cc
@@ -5,39 +5,39 @@
 
 namespace blackjack {
 
-    Deck::Deck() {
-        for (int suit = 0; suit < 4; suit++) {
+Deck::Deck() {
+    for (int suit = 0; suit < 4; suit++) {
 
-            for (int rank = 1; rank < 14; rank++) {
-                Card card = Card((Suit)suit, (Rank)rank);
-                this->deck.push_back(card);
-            }
+        for (int rank = 1; rank < 14; rank++) {
+            Card card = Card((Suit)suit, (Rank)rank);
+            this->deck.push_back(card);
         }
     }
+}
 
-    Card Deck::RemoveCard() {
-       Card card = deck.back();
-       deck.pop_back();
-       return card;
-    }
+Card Deck::RemoveCard() {
+   Card card = deck.back();
+   deck.pop_back();
+   return card;
+}
 
-    void Deck::Shuffle() {
-        // Code below assisted by: https://stackoverflow.com/questions/6926433/how-to-shuffle-a-stdvector
-        unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-        std::default_random_engine e(seed);
-        std::shuffle(std::begin(deck), std::end(deck), e);
-    }
+void Deck::Shuffle() {
+    // Code below assisted by: https://stackoverflow.com/questions/6926433/how-to-shuffle-a-stdvector
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::default_random_engine e(seed);
+    std::shuffle(std::begin(deck), std::end(deck), e);
+}
 
-    int Deck::Size() const {
-        return this->deck.size();
-    }
+int Deck::Size() const {
+    return this->deck.size();
+}
 
-    std::ostream& operator<<(std::ostream& os, Deck& deck) {
-        for (Card card : deck.deck) {
-            os << card;
-        }
-        return os;
+std::ostream& operator<<(std::ostream& os, Deck& deck) {
+    for (Card card : deck.deck) {
+        os << card;
     }
+    return os;
+}
 
 } // namespace blackjack
 
