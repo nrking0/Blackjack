b8d2f7d7b1182aa684b658e659e20cd2d1b6bc32
Nick King <nrking0@gmail.com>
2021-04-16 16:23:28 -0500

    Build up player class

--- include/player.h
+++ include/player.h
@@ -1,8 +1,30 @@
 #ifndef BLACKJACK_PLAYER_H
 #define BLACKJACK_PLAYER_H
 
+#include <string>
+#include <vector>
+#include "card.h"
+
 namespace blackjack {
 
+class Player {
+
+public:
+    Player(std::string name);
+    int GetScore() const;
+    void DealCard(Card card);
+    void ClearHand();
+    std::string GetName() const;
+    bool GetHasPlayed() const;
+    void SetHasPlayed(bool has_played);
+
+
+private:
+    std::string name_;
+    std::vector<Card> hand_;
+    bool has_played_;
+
+};
 
 } // namespace blackjack
 
